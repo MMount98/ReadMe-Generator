67022b2f3d9a5d7073acb98158f8ab43b8d5dea5
Michael Mount <mount.photos@gmail.com>
2023-03-02 18:24:42 -0700

    Added requires for fs and custom module

--- Assets/index.js
+++ Assets/index.js
@@ -58,17 +58,8 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+      console.log(error);
     }
   });
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();

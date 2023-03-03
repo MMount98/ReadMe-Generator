4ca0240fc44222ac2a2162c9e5646c2fda10acc3
Michael Mount <mount.photos@gmail.com>
2023-03-02 18:13:48 -0700

    add prompts to index.js

---
+++ Assets/index.js
@@ -0,0 +1,74 @@
+//REQUIRES
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
+
+// TODO: Create an array of questions for user input
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your gitHub username?",
+      name: "userName",
+    },
+    {
+      type: "input",
+      message: "Provide a contact Email:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Please enter Project Title:",
+      name: "title",
+    },
+    {
+      type: "list",
+      message: "Provide Licensing:",
+      choices: ["Mit", "Apache 2.0", "GPL 3.0", "BSP 3", "None"],
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Provide a Description:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Provide Installation Instructions:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Provide Usage Instructions:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Provide Contribution Instructions:",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Provide Testing Instructions:",
+      name: "test",
+    },
+  ])
+  .then((userInput) => {
+    console.log(userInput);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
